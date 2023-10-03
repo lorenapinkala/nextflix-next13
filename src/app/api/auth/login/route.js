c41c8d98acdaddbd1be0f48f5f652e3b03f3998f
lorenapinkala <baeksumin77@gmail.com>
2023-10-03 20:53:35 -0300

    login route finished

--- src/app/api/auth/login/route.js
+++ src/app/api/auth/login/route.js
@@ -1,19 +1,42 @@
-import connectDB from '@/app/libs/connectDB'
-import { NextResponse } from 'next/server'
-import  UserModel  from '@/app/models/User'
+import connectDB from "@/app/libs/connectDB";
+import { NextResponse } from "next/server";
+import UserModel from "@/app/models/User";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
+export const POST = async (req, res) => {
+  try {
+    await connectDB();
+    const body = await req.json();
+    const user = await UserModel.findOne({ email: body.email });
 
-
-export const GET= async(req, res)=> {
-
-    await connectDB()
-    try{
-        // const body = await req.json();
-        // const newUser = await UserModel.create(body);
-        return NextResponse.json({message:"hola a todos"},{status:201})
-    }catch(error){
-        console.log(error)
-        NextResponse.json({data: null}, {status:500})
+    if (!user) {
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
     }
-  
+
+    const { _id, username, email, password, salt, favorites } = user;
+    const passwordHash = bcrypt.hashSync(body.password, salt);
+
+    if (passwordHash !== password) {
+      return NextResponse.json(
+        { message: "Something went wrong" },
+        { status: 401 }
+      );
+    }
+
+    if (passwordHash === password) {
+      const token = jwt.sign({ _id, username, email }, process.env.SECRET, {
+        expiresIn: "5d",
+      });
+
+      const payload = jwt.verify(token, process.env.SECRET);
+
+      req.user = payload;
+
+      return NextResponse.json({ token, payload }, { status: 201 });
+    }
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ data: null }, { status: 500 });
   }
+};
